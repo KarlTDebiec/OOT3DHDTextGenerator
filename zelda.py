fe1eb8c2d8a57f28bc92f1498d3d3aa2a5a6a004
Karl Debiec <karl.t.debiec@gmail.com>
2020-02-02 11:41:13 -0600

    Implemented confirmation of image text

--- zelda.py
+++ zelda.py
@@ -6,18 +6,17 @@ from os.path import expandvars, isdir, isfile, basename, dirname
 import numpy as np
 import yaml
 from PIL import Image, UnidentifiedImageError
-from imgcat import imgcat
 from IPython import embed
 from collections import OrderedDict
 
 import h5py
 
 
-class ZeldaUpsaler(object):
+class ZeldaOOT3DHDTextGenerator(object):
 
     # region Builtins
 
-    def __init__(self, conf_file="conf.yaml"):
+    def __init__(self, conf_file: str = "conf.yaml"):
 
         # Read configuration
         if not (isfile(conf_file) and access(conf_file, R_OK)):
@@ -32,50 +31,26 @@ class ZeldaUpsaler(object):
         # Load cache
         self.load_cache()
 
-        # Load confirmed image cache
-        #   HDF5 file with two tables:
-        #       images/confirmed/filename (Nx36 str)
-        #       images/confirmed/text (N str)
-        #   Reconstruct data structures
-        #       confirmed_images (dict):
-        #           Keys are filenames (str)
-        #           Values are text (str)
-
         # Review all existing images
         for i, filename in enumerate(listdir(self.dump_directory)):
-            if filename in self.unconfirmed_images:
-                print(i, filename)
-                continue
-            if not (self.is_text_image(f"{self.dump_directory}/{filename}")):
-                continue
-            text_data = np.array(Image.fromarray(np.array(
-                Image.open(f"{self.dump_directory}/{filename}"))[:, :, 3]))
-            chars = []
-            for x in range(16):
-                for y in range(16):
-                    char_data = text_data[x * 16:(x + 1) * 16,
-                                y * 16:(y + 1) * 16]
-                    if char_data.tobytes() not in self.chars:
-                        self.chars[char_data.tobytes()] = ("", False)
-                    chars.append(
-                        list(self.chars.keys()).index(char_data.tobytes()))
 
-            print(i, filename)
-            self.unconfirmed_images[filename] = np.array(chars, np.uint32)
+            # If filename is known, check if confirmed or unconfirmed
+            if filename in self.confirmed_texts:
+                continue
+            elif filename in self.unconfirmed_texts:
+                if self.is_text_confirmable(filename):
+                    self.confirm_text(filename)
+                continue
+
+            # If file is not a text image, skip
+            if not self.is_file_text_image(filename):
+                continue
+
+            # Otherwise, add new text
+            self.add_text(filename)
 
         # Assign unassigned characters
-        for char, (assignment, confirmed) in self.chars.items():
-            if not confirmed:
-                image = Image.fromarray(
-                    np.frombuffer(char, dtype=np.uint8).reshape(16, 16))
-                print(np.frombuffer(char, dtype=np.uint8).reshape(16, 16))
-                imgcat(image.resize((160, 160)))
-                try:
-                    assignment = input("Assignment:")
-                    if assignment != "":
-                        self.chars[char] = (assignment, True)
-                except KeyboardInterrupt:
-                    break
+        self.assign_chars()
 
         # Save cache
         self.save_cache()
@@ -85,13 +60,13 @@ class ZeldaUpsaler(object):
     # region Properties
 
     @property
-    def cache_file(self):
+    def cache_file(self) -> str:
         if not hasattr(self, "_cache_file"):
             self._cache_file = None
         return self._cache_file
 
     @cache_file.setter
-    def cache_file(self, value):
+    def cache_file(self, value: str):
         value = expandvars(value)
         if isfile(value):
             if not (access(value, R_OK) and access(value, W_OK)):
@@ -105,139 +80,262 @@ class ZeldaUpsaler(object):
         self._cache_file = value
 
     @property
-    def chars(self):
+    def chars(self) -> OrderedDict:
         if not hasattr(self, "_chars"):
             self._chars = OrderedDict()
         return self._chars
 
     @chars.setter
-    def chars(self, value):
+    def chars(self, value: OrderedDict):
         if not (isinstance(value, OrderedDict)):
             raise ValueError()
         self._chars = value
 
     @property
-    def confirmed_images(self):
-        if not hasattr(self, "_confirmed_images"):
-            self._confirmed_images = {}
-        return self._confirmed_images
-
-    @confirmed_images.setter
-    def confirmed_images(self, value):
-        if not (isinstance(value, dict)):
-            raise ValueError()
-        self._confirmed_images = value
+    def char_bytes(self) -> list:
+        return list(self.chars.keys())
 
     @property
-    def dump_directory(self):
+    def char_confirmations(self) -> list:
+        return [c[1] for c in self.chars.values()]
+
+    @property
+    def char_assignments(self) -> list:
+        return [c[0] for c in self.chars.values()]
+
+    @property
+    def char_images(self) -> np.ndarray:
+        return np.stack([np.frombuffer(k, dtype=np.uint8)
+                         for k in self.char_bytes]).reshape((-1, 16, 16))
+
+    @property
+    def confirmed_texts(self) -> dict:
+        if not hasattr(self, "_confirmed_texts"):
+            self._confirmed_texts = {}
+        return self._confirmed_texts
+
+    @confirmed_texts.setter
+    def confirmed_texts(self, value: dict):
+        if not (isinstance(value, dict)):
+            raise ValueError()
+        self._confirmed_texts = value
+
+    @property
+    def dump_directory(self) -> str:
         if not hasattr(self, "_dump_directory"):
             raise ValueError()
         return self._dump_directory
 
     @dump_directory.setter
-    def dump_directory(self, value):
+    def dump_directory(self, value: str):
         value = expandvars(value)
         if not (isdir(value) and access(value, R_OK)):
             raise ValueError()
         self._dump_directory = value
 
     @property
-    def unconfirmed_images(self):
-        if not hasattr(self, "_unconfirmed_images"):
-            self._unconfirmed_images = {}
-        return self._unconfirmed_images
+    def unconfirmed_texts(self) -> dict:
+        if not hasattr(self, "_unconfirmed_texts"):
+            self._unconfirmed_texts = {}
+        return self._unconfirmed_texts
 
-    @unconfirmed_images.setter
-    def unconfirmed_images(self, value):
+    @unconfirmed_texts.setter
+    def unconfirmed_texts(self, value: dict):
         if not (isinstance(value, dict)):
             raise ValueError()
-        self._unconfirmed_images = value
+        self._unconfirmed_texts = value
+
+    @property
+    def verbosity(self) -> int:
+        """int: Level of output to provide"""
+        if not hasattr(self, "_verbosity"):
+            self._verbosity = 1
+        return self._verbosity
+
+    @verbosity.setter
+    def verbosity(self, value: int):
+        if not isinstance(value, int) and value >= 0:
+            raise ValueError()
+        self._verbosity = value
 
     # endregion
 
-    # region Public Methods
+    # region Methods
 
-    def load_cache(self):
-        with h5py.File(self.cache_file) as f:
+    def add_text(self, filename: str) -> None:
+        if filename in self.confirmed_texts:
+            raise ValueError()
+        elif filename in self.unconfirmed_texts:
+            raise ValueError()
+        text_data = np.array(Image.open(f"{self.dump_directory}/{filename}"))
+        text_data = text_data[:, :, 3]
+        indexes = []
+        for x in range(16):
+            for y in range(16):
+                char_data = text_data[x * 16:(x + 1) * 16, y * 16:(y + 1) * 16]
+                char_bytes = char_data.tobytes()
+                if char_bytes not in self.chars:
+                    self.chars[char_bytes] = ("", False)
+                indexes.append(self.char_bytes.index(char_bytes))
+        self.unconfirmed_texts[filename] = np.array(indexes, np.uint32)
+        if self.is_text_confirmable(filename):
+            self.confirm_text(filename)
+
+    def assign_chars(self) -> None:
+        for char, (assignment, confirmed) in self.chars.items():
+            if not confirmed:
+                data = np.frombuffer(char, dtype=np.uint8).reshape(16, 16)
+                print(data)
+                image = Image.fromarray(data).resize((160, 160), Image.NEAREST)
+                self.show_image(image)
+                try:
+                    assignment = input("Assignment:")
+                    if assignment != "":
+                        self.chars[char] = (assignment, True)
+                except KeyboardInterrupt:
+                    break
+
+    def confirm_text(self, filename: str) -> None:
+        if filename in self.confirmed_texts:
+            raise ValueError()
+        elif filename not in self.unconfirmed_texts:
+            raise ValueError()
+
+        self.confirmed_texts[filename] = self.get_text(filename)
+        del self.unconfirmed_texts[filename]
+
+    def get_text(self, filename: str) -> str:
+        if filename not in self.unconfirmed_texts:
+            raise ValueError()
+
+        indexes = self.unconfirmed_texts[filename]
+        assignments = [self.char_assignments[i] for i in indexes]
+        return "".join(assignments).rstrip()
+
+    def is_file_text_image(self, filename: str) -> bool:
+        try:
+            data = np.array(Image.open(f"{self.dump_directory}/{filename}"))
+        except UnidentifiedImageError:
+            return False
+        if data.shape != (256, 256, 4):
+            return False
+        if data[:, :, :3].sum() != 0:
+            return False
+        return True
+
+    def is_text_confirmable(self, filename: str) -> bool:
+        if filename not in self.unconfirmed_texts:
+            raise ValueError()
+
+        indexes = self.unconfirmed_texts[filename]
+        confirmations = [self.char_confirmations[i] for i in indexes]
+        if np.all(confirmations):
+            return True
+        else:
+            return False
+
+    def load_cache(self) -> None:
+
+        with h5py.File(self.cache_file) as cache:
+
             # Load characters
-            if "characters" in f:
+            if "characters" in cache:
                 assignments = [a.decode("UTF8") for a in
-                               np.array(f["characters/assignments"])]
-                confirmations = np.array(f["characters/confirmations"])
-                images = np.array(f["characters/images"])
+                               np.array(cache["characters/assignments"])]
+                confirmations = np.array(cache["characters/confirmations"])
+                images = np.array(cache["characters/images"])
                 for i, a, c in zip(images, assignments, confirmations):
                     self.chars[i.tobytes()] = (a, c)
 
             # Load unconfirmed texts
-            if "texts/unconfirmed" in f:
+            if "texts/unconfirmed" in cache:
                 filenames = [f.decode("UTF8") for f in
-                             np.array(f["texts/unconfirmed/filenames"])]
-                indexes = np.array(f["texts/unconfirmed/indexes"])
-                for f, i in zip(filenames, indexes):
-                    self.unconfirmed_images[f] = i
+                             np.array(cache["texts/unconfirmed/filenames"])]
+                indexes = np.array(cache["texts/unconfirmed/indexes"])
+                for cache, i in zip(filenames, indexes):
+                    self.unconfirmed_texts[cache] = i
 
             # Load confirmed texts
+            if "texts/confirmed" in cache:
+                filenames = [f.decode("UTF8") for f in
+                             np.array(cache["texts/confirmed/filenames"])]
+                texts = [f.decode("UTF8") for f in
+                         np.array(cache["texts/confirmed/text"])]
+                for f, t in zip(filenames, texts):
+                    self.confirmed_texts[f] = t
 
-    def is_text_image(self, path):
-        if basename(path).split("_")[1] != "256x256":
-            return False
-        try:
-            rgba_data = np.array(Image.open(path))
-        except UnidentifiedImageError:
-            return False
-        if rgba_data.shape != (256, 256, 4):
-            return False
-        if rgba_data[:, :, :3].sum() != 0:
-            return False
-        return True
+    def save_cache(self) -> None:
 
-    def save_cache(self):
-        with h5py.File(self.cache_file) as f:
+        with h5py.File(self.cache_file) as cache:
+
             # Save characters
-            if "characters" in f:
-                del f["characters"]
-            assignments = [v[0].encode("UTF8") for v in self.chars.values()]
-            f.create_dataset("characters/assignments",
-                             data=assignments,
-                             dtype="S1",
-                             chunks=True,
-                             compression="gzip")
-            confirmations = [v[1] for v in self.chars.values()]
-            f.create_dataset("characters/confirmations",
-                             data=confirmations,
-                             dtype=bool,
-                             chunks=True,
-                             compression="gzip")
-            images = np.stack(
-                [np.frombuffer(k, dtype=np.uint8) for k in self.chars.keys()])
-            images = images.reshape((-1, 16, 16))
-            f.create_dataset("characters/images",
-                             data=images,
-                             dtype=np.uint8,
-                             chunks=True,
-                             compression="gzip")
+            if "characters" in cache:
+                del cache["characters"]
+            cache.create_dataset("characters/assignments",
+                                 data=[a.encode("UTF8") for a in
+                                       self.char_assignments],
+                                 dtype="S4",
+                                 chunks=True,
+                                 compression="gzip")
+            cache.create_dataset("characters/confirmations",
+                                 data=self.char_confirmations,
+                                 dtype=bool,
+                                 chunks=True,
+                                 compression="gzip")
+            cache.create_dataset("characters/images",
+                                 data=self.char_images,
+                                 dtype=np.uint8,
+                                 chunks=True,
+                                 compression="gzip")
 
             # Save unconfirmed texts
-            if "texts/unconfirmed" in f:
-                del f["texts/unconfirmed"]
-            filenames = [k.encode("UTF8")
-                         for k in self.unconfirmed_images.keys()]
-            f.create_dataset("texts/unconfirmed/filenames",
-                             data=filenames,
-                             dtype="S255",
-                             chunks=True,
-                             compression="gzip")
-            indexes = np.stack(list(self.unconfirmed_images.values()))
-            f.create_dataset("texts/unconfirmed/indexes",
-                             data=indexes,
-                             dtype=np.uint32,
-                             chunks=True,
-                             compression="gzip")
+            if "texts/unconfirmed" in cache:
+                del cache["texts/unconfirmed"]
+            if len(self.unconfirmed_texts) > 0:
+                cache.create_dataset("texts/unconfirmed/filenames",
+                                     data=[k.encode("UTF8") for k in
+                                           self.unconfirmed_texts.keys()],
+                                     dtype="S48",
+                                     chunks=True,
+                                     compression="gzip")
+                cache.create_dataset("texts/unconfirmed/indexes",
+                                     data=np.stack(
+                                         list(
+                                             self.unconfirmed_texts.values())),
+                                     dtype=np.uint32,
+                                     chunks=True,
+                                     compression="gzip")
 
             # Save confirmed texts
+            if "texts/confirmed" in cache:
+                del cache["texts/confirmed"]
+            if len(self.confirmed_texts) > 0:
+                cache.create_dataset("texts/confirmed/filenames",
+                                     data=[k.encode("UTF8") for k in
+                                           self.confirmed_texts.keys()],
+                                     dtype="S48",
+                                     chunks=True,
+                                     compression="gzip")
+                cache.create_dataset("texts/confirmed/text",
+                                     data=[t.encode("UTF8") for t in
+                                           self.confirmed_texts.values()],
+                                     dtype="S1024",
+                                     chunks=True,
+                                     compression="gzip")
+
+    # Static Methods
+
+    @staticmethod
+    def show_image(image: Image.Image) -> None:
+        try:
+            from imgcat import imgcat
+
+            imgcat(image)
+        except ImportError:
+            image.show()
 
     # endregion
 
 
 if __name__ == "__main__":
-    ZeldaUpsaler()()
+    ZeldaOOT3DHDTextGenerator()()
