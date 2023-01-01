eb33e5c474f5af6167ee576327f38aa859846e05
Karl T Debiec <karl.t.debiec@gmail.com>
2023-01-01 14:06:19 -0500

    wip

--- oot3dhdtextgenerator/core/__init__.py
+++ oot3dhdtextgenerator/core/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  Copyright 2020-2022 Karl T Debiec
+#  Copyright 2020-2023 Karl T Debiec
 #  All rights reserved. This software may be modified and distributed under
 #  the terms of the BSD license. See the LICENSE file for details.
 """Core code."""
