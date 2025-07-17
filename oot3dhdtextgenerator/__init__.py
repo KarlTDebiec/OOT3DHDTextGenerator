6a63e58368446c33de45a569aa6ca795bf52c4d0
Karl T Debiec <karl.t.debiec@gmail.com>
2025-07-17 08:14:07 -0400

    Insert future import (#147)

--- oot3dhdtextgenerator/__init__.py
+++ oot3dhdtextgenerator/__init__.py
@@ -1,3 +1,5 @@
 #  Copyright 2020-2025 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """OOT3D HD Text Generator."""
+
+from __future__ import annotations
