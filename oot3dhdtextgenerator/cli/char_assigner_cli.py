de555931799da2aaed9d6940d13f2487178648ab
Karl Debiec <karl.t.debiec@gmail.com>
2025-07-17 06:42:21 -0400

    Update copyright years from 2023 to 2025 in multiple files

--- oot3dhdtextgenerator/cli/char_assigner_cli.py
+++ oot3dhdtextgenerator/cli/char_assigner_cli.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  Copyright 2020-2023 Karl T Debiec. All rights reserved. This software may be modified
+#  Copyright 2020-2025 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Character assigner command-line interface."""
 from __future__ import annotations
