02fd9991b46e1fdfac93f1165001c660429edd01
Karl T Debiec <karl.t.debiec@gmail.com>
2026-01-24 09:44:44 -0500

    Update copyright (#170)

--- oot3dhdtextgenerator/utilities/__init__.py
+++ oot3dhdtextgenerator/utilities/__init__.py
@@ -1,6 +1,7 @@
-#  Copyright 2020-2025 Karl T Debiec. All rights reserved. This software may be modified
+#  Copyright 2020-2026 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Utilities."""
+
 from __future__ import annotations
 
 from oot3dhdtextgenerator.utilities.learning_dataset_generator import (
