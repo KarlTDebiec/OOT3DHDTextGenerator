2c5e291c379f401ee740597ef9c9c30e064f38c2
Karl Debiec <karl.t.debiec@gmail.com>
2026-01-24 09:42:14 -0500

    wip

--- oot3dhdtextgenerator/apps/char_assigner/__init__.py
+++ oot3dhdtextgenerator/apps/char_assigner/__init__.py
@@ -1,6 +1,5 @@
-#  Copyright 2020-2025 Karl T Debiec. All rights reserved. This software may be
-#  modified and distributed under the terms of the BSD license. See the LICENSE
-#  file for details.
+#  Copyright 2020-2026 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Character assigner Flask application."""
 
 from __future__ import annotations
